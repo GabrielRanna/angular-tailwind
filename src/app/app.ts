6e6d296f37a90fe298961ba135a2a6963433a8bc
kenstudiobrasuka <ken@studiobrasuka.com.br>
2025-06-21 20:15:03 -0300

    fix

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   imports: [],
@@ -8,4 +9,7 @@ import { Component } from '@angular/core';
 })
 export class App {
   protected title = 'angular-tailwind';
+}
+export class AppComponent {
+  title = 'angular-tailwind';
 }
