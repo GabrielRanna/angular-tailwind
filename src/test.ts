5778805aa89ee1e321030fae5a9ee47cabd5dc4f
kenstudiobrasuka <ken@studiobrasuka.com.br>
2025-06-22 01:07:06 -0300

    fk

---
+++ src/test.ts
@@ -0,0 +1,13 @@
+﻿// test.ts
+import 'zone.js/testing';
+import { TestBed } from '@angular/core/testing';
+import {
+  BrowserDynamicTestingModule,
+  platformBrowserDynamicTesting
+} from '@angular/platform-browser-dynamic/testing';
+
+// Inicialização do ambiente de teste
+TestBed.initTestEnvironment(
+  BrowserDynamicTestingModule,
+  platformBrowserDynamicTesting(),
+);
